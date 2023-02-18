0e72235c1a2085e1542ce321a59030f716a419fa
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-02-18 20:33:50 -0300

    create CRUD for products

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AppService } from './app.service';
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
     }),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
