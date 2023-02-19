2f87ab0363312267ffb2ff3e234fcbd23bf63d75
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-02-19 14:08:22 -0300

    dependecies and configure cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
 
   app.useGlobalPipes(
     new ValidationPipe({
